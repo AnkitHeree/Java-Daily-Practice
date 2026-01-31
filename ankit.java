b1c67735323cf06ba76798c6015c68ea4ecc19da
AnkitHeree <ankityadav9082384811@gmail.com>
2026-01-31 19:20:19 +0530

    first Program Day 1

---
+++ ankit.java
@@ -0,0 +1,15 @@
+/*Taking input from user and Print That Input */
+
+import java.util.*;
+public class ankit {
+public static void main(String[] args) {
+    int a;
+    System.out.println("Enter Any Number");
+    Scanner sc = new Scanner(System.in);
+    a=sc.nextInt();
+
+    System.out.println("The Number Is \n" +a);
+
+}
+    
+}
