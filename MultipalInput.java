57ae91878fcf52c5aaf008236346a01d4dd7402c
AnkitHeree <ankityadav9082384811@gmail.com>
2026-01-31 19:37:53 +0530

    2 Input Second Program Day 1

---
+++ MultipalInput.java
@@ -0,0 +1,20 @@
+/*Taking 2 Input from User And Print Int Anbd String */
+
+import java.util.*;
+public class MultipalInput {
+   public static void main(String[] args) {
+    Scanner fr = new Scanner(System.in);
+    int age;
+    String Name;
+
+    System.out.println("Enter Your Age");
+    age= fr.nextInt();
+    fr.nextLine();
+
+    System.out.println("Enter Your Name");
+    Name=fr.nextLine();
+
+    System.out.println("Your Age Is :" + age);
+    System.out.println("Your Name Is :" + Name);
+   } 
+}
