1a79fca2323f12f61dbf74e98029d4744469abfe
AnkitHeree <ankityadav9082384811@gmail.com>
2026-02-01 14:26:15 +0530

    Day 2 Area of Circle

---
+++ AreaofCircle.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class AreaofCircl {
+
+    public static void main(String[] args) {
+        double radius;
+        double area;
+
+        Scanner red =new Scanner(System.in);
+
+        System.out.println("Enter Radious :");
+        radius = red.nextDouble();
+
+        area = 3.14*radius*radius;
+        System.out.println("The Area of Circle is : "+ area);
+        red.close();
+
+    }
+}
